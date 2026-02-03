025552bbf683cc5d489d1087375f5eb31c1944e0
codefulTM <147736560+codefulTM@users.noreply.github.com>
2026-02-03 11:51:45 +0700

    feat: add Test module, controller, and service for improved testing functionality

--- 02_Source/05_Proof of Concept/inventory-backend/src/app.module.ts
+++ 02_Source/05_Proof of Concept/inventory-backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module'
+import { TestModule } from './test/test.module' 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TestModule],
   controllers: [AppController],
   providers: [AppService],
 })
