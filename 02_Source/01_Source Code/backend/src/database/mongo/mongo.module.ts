b7bd2a2491257036d482940e769a65fd8428942f
Nguyen Thai Tan <nguyenthaitan9@gmail.com>
2026-02-06 16:43:07 +0700

    update docker file

--- 02_Source/01_Source Code/backend/src/database/mongo/mongo.module.ts
+++ 02_Source/01_Source Code/backend/src/database/mongo/mongo.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI') || 'mongodb://admin:password123@localhost:27017/inventory_management_db?authSource=admin',
+        uri: configService.get<string>('MONGO_URI') || 'mongodb+srv://admin:123@inventorymanagement.kbyjdmp.mongodb.net/?appName=InventoryManagement',
       }),
       inject: [ConfigService],
     }),
