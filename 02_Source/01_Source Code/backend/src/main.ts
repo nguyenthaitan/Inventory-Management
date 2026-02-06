5655b8b721d7e9d7b9931129c7086a9ee94d7a29
Nguyen Thai Tan <nguyenthaitan9@gmail.com>
2026-02-06 17:00:18 +0700

    update docker file

--- 02_Source/01_Source Code/backend/src/main.ts
+++ 02_Source/01_Source Code/backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Allow Frontend to communicate
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Backend is running on: ${await app.getUrl()}`);
 }
 bootstrap();
