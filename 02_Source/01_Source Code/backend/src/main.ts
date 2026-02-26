bf66b7fafb55bba49957dbf36c5622f3bbb0c8f4
codefulTM <147736560+codefulTM@users.noreply.github.com>
2026-02-26 20:51:42 +0700

    feat: update server port configuration to use ConfigService for better environment management

--- 02_Source/01_Source Code/backend/src/main.ts
+++ 02_Source/01_Source Code/backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Allow Frontend to communicate
-  await app.listen(process.env.PORT || 3000);
+
+  // retrieve port from configuration service (reads from env or other sources)
+  const config = app.get(ConfigService);
+  const port = config.get<string>('PORT') ?? '3000';
+  await app.listen(parseInt(port, 10));
+
   console.log(`Backend is running on: ${await app.getUrl()}`);
 }
 bootstrap();
