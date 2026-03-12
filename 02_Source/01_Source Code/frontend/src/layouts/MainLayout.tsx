5f8e57fc84a13157f96363e105da3aa07d9b1ed0
Nguyen Thai Tan <nguyenthaitan9@gmail.com>
2026-03-13 03:46:17 +0700

    fix: merge

--- 02_Source/01_Source Code/frontend/src/layouts/MainLayout.tsx
+++ 02_Source/01_Source Code/frontend/src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { useState, type ReactNode } from "react";
-import { Link, useNavigate, useLocation, Outlet, Navigate } from "react-router-dom";
+import { Link, useNavigate, useLocation, Outlet } from "react-router-dom";
 import {
   LayoutDashboard,
   Package,
