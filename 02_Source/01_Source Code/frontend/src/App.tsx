023e6cc5cfdba0270ed509e5fa8ee4a496e46926
Nguyen Thai Tan <nguyenthaitan9@gmail.com>
2026-03-13 16:49:00 +0700

    fix: test

--- 02_Source/01_Source Code/frontend/src/App.tsx
+++ 02_Source/01_Source Code/frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./App.css"
 
-functio App() {
+function App() {
   return <RouterProvider router={router} />;
 }
 
